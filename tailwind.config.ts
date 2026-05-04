b4cf1a96d0d3027e7f236bc5535d0707061bb079
Koat-1 <kessentini.youssef03@gmail.com>
2026-05-04 12:58:29 +0100

    config change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // This path is now correct for root index.html
   theme: {
     extend: {},
   },
