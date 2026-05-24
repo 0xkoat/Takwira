936abf83f4513fb4d68bc70d682763c1ad7b887f
0xkoat <kessentini.youssef03@gmail.com>
2026-05-24 15:56:13 +0100

    typo correction

--- src/types/Stadium.tsx
+++ src/types/Stadium.tsx
@@ -9,3 +9,12 @@ export interface Stadium {
   price: string;
   placesNum: string;
 }
+
+export interface StadiumCardProps {
+  stadium: Stadium;
+}
+
+export interface StadiumsListProps {
+  stadiums: Stadium[];
+}
+
