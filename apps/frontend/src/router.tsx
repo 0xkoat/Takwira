93c538d192438d4f328c5a972d7fec65cca5e704
0xkoat <kessentini.youssef03@gmail.com>
2026-06-05 11:46:42 +0100

    removed manual implementation of route tree

--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -1,23 +1,11 @@
-import { createBrowserHistory, Router } from '@tanstack/react-router';
-import { Route as RootRoute } from './routes/__root';
-import { Route as IndexRoute } from './routes/index';
-import { Route as SignupRoute } from './routes/signup';
-import { Route as StadiumsRoute } from './routes/stadiums';
-import { Route as ProfileRoute } from './routes/profile';
-import { Route as PostStadiumRoute } from './routes/post-stadiums';
+import { createBrowserHistory, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
 
 const history = createBrowserHistory({ window });
 
-const routeTree = RootRoute.addChildren([
-  IndexRoute,
-  SignupRoute,
-  StadiumsRoute,
-  ProfileRoute,
-  PostStadiumRoute,
-]);
-
-export const router = new Router({
+export const router = createRouter({
   routeTree,
   history,
   defaultPreload: 'intent',
 });
+
